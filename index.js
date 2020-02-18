2bfe6abad5d1b6dd95fae1ad93fc426a35cd28ff
ameba23 <ameba23@systemli.org>
2020-02-18 14:33:44 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const sodium = require('sodium-native')
+const bip39 = require('bip39')
+const assert = require('assert')
+
+module.exports = (key, options = {}) => {
+  assert(Buffer.isBuffer(key), 'Key must be a buffer')
+  const numWords = options.numWords || 2
+  const language = options.language || 'english'
+  const wordlist = options.wordlist || bip39.wordlists[language]
+  const hash = genericHash(key)
+  let words = ''
+  for (let i = 0; i < numWords; i++) {
+    const num = hash.readUInt32LE(i) % 2048
+    const word = wordlist[num]
+    words += word + ' '
+  }
+  return words
+}
+
+
+function genericHash (msg, key) {
+  const hash = sodium.sodium_malloc(sodium.crypto_generichash_BYTES)
+  sodium.crypto_generichash(hash, msg, key)
+  return hash
+}
